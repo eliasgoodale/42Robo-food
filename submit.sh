fd9e1c935539788869f6cc23cacf42a65ad59b61
Elias Goodale <egoodale@e1z3r4p15.42.us.org>
2019-03-01 11:57:11 -0800

    adding scripts

--- submit.sh
+++ submit.sh
@@ -16,6 +16,15 @@ gcloud ml-engine jobs submit training $JOB_NAME \
     --region us-central1 \
     --config config.yaml \
     --runtime-version 1.10 \
-    --python-version 3.5
-    --output-dir $BUCKET"pizza_agent_$now" \
+    --python-version 3.5 \
+    -- \
+    --rows 12 \
+    --columns 12 \
+    --n-epoch 5000 \
+    --n-episodes 200 \
+    --max-steps 1440 \
     --learning-rate 0.0005 \
+    --output-dir $BUCKET"pizza_agent_$now" \
+    --gamma 0.95 \
+    --min-ingred 1 \
+    --max-ingred 6 \
